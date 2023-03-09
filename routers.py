d1dd9530d82eee5c86ddec1a3fa3d028614fe2d4
Felipe <tomazalvesfelipe@gmail.com>
2023-03-09 15:28:57 -0400

    FEAT: validação de query do preço e moedas

--- routers.py
+++ routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Query
 from converter import sync_converter, async_converter
 from asyncio import gather
 
@@ -17,8 +17,8 @@ router = APIRouter(prefix='/converter')
 @router.get("/{from_currency}")
 def converter(
     from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$'),
-    to_currencies: str,
-    price: float
+    to_currencies: str = Query(max_length=50, regex='^[A-Z]{3}(,[A-Z]{3})*$'),
+    price: float = Query(gt=0)
 ):
     to_currencies = to_currencies.split(',')
 
